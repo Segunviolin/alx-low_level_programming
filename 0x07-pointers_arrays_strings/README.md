2e786298fd3040c4b56688d2f4b8103a2b1b2227
Segunviolin <verifiedolusegun@gmail.com>
2022-09-27 13:02:21 -0700

    task 0

--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-0x07-pointers_arrays_strings
+even more pointers and array
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);v
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
@@ -13,5 +13,3 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif
-
-
